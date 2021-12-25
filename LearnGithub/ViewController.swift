b243d9fe55bd426757fa97bfa5d5499a4eba7fdb
Dhruv Reddy <dhruv.reddy700@gmail.com>
2021-12-24 18:40:24 -0800

    first manual commit

--- LearnGithub/ViewController.swift
+++ LearnGithub/ViewController.swift
@@ -12,6 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+            let label = UILabel()
+            label.text = "label"
+            label.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(label)
+            
+            // Create constraints
+            let centerX = NSLayoutConstraint(item: label, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
+            let centerY = NSLayoutConstraint(item: label, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)
+            
+            view.addConstraints([centerX, centerY])
+        
+        
+        
     }
 
 
