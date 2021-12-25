d6700c02d1fb69c8dc02738b297e2b5e5d065dec
Dhruv Reddy <dhruv.reddy700@gmail.com>
2021-12-24 19:13:57 -0800

    second manual commit

---
+++ LearnGithub/ModelViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ModelViewController.swift
+//  LearnGithub
+//
+//  Created by Dhruv Reddy on 12/24/21.
+//
+
+import UIKit
+
+class ModelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
--- LearnGithub/ViewController.swift
+++ LearnGithub/ViewController.swift
@@ -24,7 +24,6 @@ class ViewController: UIViewController {
             view.addConstraints([centerX, centerY])
         
         
-        
     }
 
 
